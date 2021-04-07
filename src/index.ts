03e67d171880a2dce8250a1116ddca3131a7c549
pakorek <brice.darmenia@sfr.fr>
2021-04-07 11:56:15 +0200

    update README and sort files

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
 import 'reflect-metadata';
-import { User } from './entity/User';
-import { Quiz } from './entity/Quiz';
-import { Answer } from './entity/Answer';
 import { Upload } from './entity/Upload';
 import { Course } from './entity/Course';
-import { Question } from './entity/Question';
-import { Promotion } from './entity/Promotion';
 import { Tag } from './entity/Tag';
-import { Evaluation } from './entity/Evaluation';
-import { ContactInformation } from './entity/ContactInformation';
 import { CourseResolver } from './resolvers/Course';
-import { UserResolver } from './resolvers/User';
-import { QuizResolver } from './resolvers/Quiz';
-import { passwordAuthChecker } from './utils/auth-checker';
+import { TagResolver } from './resolvers/Tag';
 import { createConnection } from 'typeorm';
 import { buildSchema } from 'type-graphql';
 import { ApolloServer } from 'apollo-server-express';
@@ -20,9 +11,6 @@ import Express from 'express';
 import dotenv from 'dotenv';
 import cors = require('cors');
 import cookieParser = require('cookie-parser');
-import { QuestionResolver } from './resolvers/Question';
-import { AnswerResolver } from './resolvers/Answer';
-import { TagResolver } from './resolvers/Tag';
 
 dotenv.config();
 
@@ -35,15 +23,8 @@ const startServer = async () => {
     password: process.env.DB_PASS,
     database: 'ELI',
     entities: [
-      User,
       Course,
-      Promotion,
       Upload,
-      Evaluation,
-      ContactInformation,
-      Quiz,
-      Question,
-      Answer,
       Tag,
     ],
     synchronize: true,
@@ -55,14 +36,9 @@ const startServer = async () => {
 
   const schema = await buildSchema({
     resolvers: [
-      UserResolver,
       CourseResolver,
-      QuizResolver,
-      QuestionResolver,
-      AnswerResolver,
       TagResolver,
     ],
-    authChecker: passwordAuthChecker,
     nullableByDefault: true,
   });
 
